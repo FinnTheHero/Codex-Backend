4f7b368790bd9c9dd333b747b5aaf73c7acc5945
FinnTheHero <aleksandre.nozadze.1@iliauni.edu.ge>
2024-08-04 13:20:56 +0400

    v1

---
+++ api/server/methods/createItem.go
@@ -0,0 +1,11 @@
+package server_methods
+
+import "github.com/gin-gonic/gin"
+
+func CreateNovel(c *gin.Context) {
+	// TODO
+}
+
+func CreateChapter(c *gin.Context) {
+	// TODO
+}
---
+++ api/server/methods/getItem.go
@@ -0,0 +1,21 @@
+package server_methods
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func GetAllNovels(c *gin.Context) {
+	// TODO
+}
+
+func GetNovel(c *gin.Context) {
+	// TODO
+}
+
+func GetAllChapters(c *gin.Context) {
+	// TODO
+}
+
+func GetChapter(c *gin.Context) {
+	// TODO
+}
