9140ac9db1ade15b4ca7eba22a04342aa3b52d0e
FinnTheHero <aleksandre.nozadze.1@iliauni.edu.ge>
2024-09-24 18:33:20 +0400

    Update token_handler.go
    Added code for validation handler.

--- api/server/handlers/auth/token_handler.go
+++ api/server/handlers/auth/token_handler.go
@@ -1,7 +1,35 @@
 package auth_handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"Codex-Backend/api/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func ValidateToken(c *gin.Context) {
+	var user models.User
 
+	// Get user from context
+	result, ok := c.Get("user")
+	if !ok {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "User not found",
+		})
+		return
+	}
+
+	// Cast user to User struct
+	user, ok = result.(models.User)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Type assertion failed",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"user": user,
+	})
+	return
 }
