fbbbe6688fd7b9aee1e00813d1dea7230adf9778
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2025-07-08 15:41:41 +0400

    Updated to fit without the DTO.

--- api/internal/usecases/auth/login.go
+++ api/internal/usecases/auth/login.go
@@ -10,30 +10,23 @@ func LoginUser(credentials domain.Credentials) (string, domain.User, error) {
 	var user domain.User
 
 	// Check if user exists
-	result, err := repository.GetUser(credentials.Email)
+	user, err := repository.GetUser(credentials.Email)
 	if err != nil {
 		return "", user, err
 	}
 
-	userDTO, ok := result.(domain.UserDTO)
-	if !ok {
-		return "", user, errors.New("Type assertion failed")
-	}
-
-	user = userDTO.User
-
-	if userDTO.Email == "" {
+	if user.Email == "" {
 		return "", user, errors.New("User not found")
 	}
 
 	// Check if password is correct
-	err = VerifyPassword(userDTO.User.Password, credentials.Password)
+	err = VerifyPassword(user.Password, credentials.Password)
 	if err != nil {
 		return "", user, errors.New("Incorrect password")
 	}
 
 	// Generate token
-	token, err := GenerateToken(userDTO.User.Email)
+	token, err := GenerateToken(user.Email)
 	if err != nil {
 		return "", user, errors.New("Error generating token: " + err.Error())
 	}
