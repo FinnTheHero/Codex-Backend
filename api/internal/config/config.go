fb1a9b4177a1b564a7b7f39a425d76609779ffec
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2025-07-08 15:35:31 +0400

    I forgot what i changed here exactly.

--- api/internal/config/config.go
+++ api/internal/config/config.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -9,34 +8,10 @@ import (
 	"github.com/joho/godotenv"
 )
 
-
 type AWSKeys struct {
-	AccessKey       string `json:"access_key"`
-	SecretKey       string `json:"secret_key"`
-	Region          string `json:"region"`
-}
-
-func GetAWSKeys() (AWSKeys, error) {
-	accessKey, err := GetEnvVariable("AWS_ACCESS_KEY")
-	if err != nil {
-		return AWSKeys{}, err
-	}
-	secretKey, err := GetEnvVariable("AWS_SECRET_KEY")
-	if err != nil {
-		return AWSKeys{}, err
-	}
-	region, err := GetEnvVariable("AWS_REGION")
-	if err != nil {
-		return AWSKeys{}, err
-	}
-
-	log.Println("AWS Env Variables Set")
-
-	return AWSKeys{
-		AccessKey:       accessKey,
-		SecretKey:       secretKey,
-		Region:          region,
-	}, nil
+	AccessKey string `json:"access_key"`
+	SecretKey string `json:"secret_key"`
+	Region    string `json:"region"`
 }
 
 func LoadEnvVariables() error {
@@ -46,8 +21,31 @@ func LoadEnvVariables() error {
 func GetEnvVariable(v string) (string, error) {
 	env_variable := os.Getenv(v)
 	if env_variable == "" {
-		return "", errors.New(fmt.Sprintf("Environmental Variable \"%v\" Not Found", v))
+		return "", fmt.Errorf("Environmental Variable \"%v\" Not Found", v)
 	}
 
 	return env_variable, nil
+}
+
+func GetAWSKeys() (AWSKeys, error) {
+	accessKey, err := GetEnvVariable("AWS_ACCESS_KEY")
+	if err != nil {
+		return AWSKeys{}, fmt.Errorf("Error getting %s: %v", accessKey, err)
+	}
+	secretKey, err := GetEnvVariable("AWS_SECRET_KEY")
+	if err != nil {
+		return AWSKeys{}, fmt.Errorf("Error getting %s: %v", secretKey, err)
+	}
+	region, err := GetEnvVariable("AWS_REGION")
+	if err != nil {
+		return AWSKeys{}, fmt.Errorf("Error getting %s: %v", region, err)
+	}
+
+	log.Println("AWS Env Variables Set")
+
+	return AWSKeys{
+		AccessKey: accessKey,
+		SecretKey: secretKey,
+		Region:    region,
+	}, nil
 }
