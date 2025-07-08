0a8b070c4b52fbbf1864b67a2670ed8ac036adab
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2025-07-08 15:51:12 +0400

    Updated the logic for the new table structure.

--- api/internal/usecases/auth/register.go
+++ api/internal/usecases/auth/register.go
@@ -9,21 +9,19 @@ import (
 
 func RegisterUser(credentials domain.NewUser) error {
 
-	var user domain.User
-	user.Email = credentials.Email
-	user.Username = credentials.Username
+	var newUser domain.User
+	newUser.Email = credentials.Email
+	newUser.Username = credentials.Username
 
 	// Check if user already exists
-	result, err := repository.GetUser(credentials.Email)
+	user, err := repository.GetUser(credentials.Email)
 	if err != nil {
 		if err.Error() != "User not found" {
 			return err
 		}
 	}
 
-	userDTO, _ := result.(domain.UserDTO)
-
-	if userDTO.Email == credentials.Email || userDTO.Email != "" {
+	if user.Email == credentials.Email || user.Email != "" {
 		return errors.New("Email already in use")
 	}
 
@@ -32,15 +30,20 @@ func RegisterUser(credentials domain.NewUser) error {
 	if err != nil {
 		return errors.New("Error hashing password: " + err.Error())
 	}
-	user.Password = string(hashedPassword)
+	newUser.Password = string(hashedPassword)
 
 	// Set user defaults
-	user.Type = "user"
-	user.Created_at = time.Now().Format("2006-01-02 15:04:05")
-	user.Updated_at = user.Created_at
+	newUser.Type = "user"
+	newUser.CreatedAt = time.Now().Format("2006-01-02 15:04:05")
+	newUser.UpdatedAt = time.Now().Format("2006-01-02 15:04:05")
+
+	newUser.ID, err = GenerateID("user")
+	if err != nil {
+		return errors.New("Error generating user ID: " + err.Error())
+	}
 
 	// Create new user
-	err = repository.CreateUser(user)
+	err = repository.CreateUser(newUser)
 	if err != nil {
 		return errors.New("Error creating user: " + err.Error())
 	}
