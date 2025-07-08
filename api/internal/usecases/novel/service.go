9a5be03598883ca112a361038506ddad1b20257b
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2025-07-08 15:50:44 +0400

    Moved some code to this file.

--- api/internal/usecases/novel/service.go
+++ api/internal/usecases/novel/service.go
@@ -3,18 +3,55 @@ package novel_service
 import (
 	"Codex-Backend/api/internal/domain"
 	"Codex-Backend/api/internal/infrastructure/repository"
+	"Codex-Backend/api/internal/infrastructure/table"
+	auth_service "Codex-Backend/api/internal/usecases/auth"
 	"errors"
+	"time"
 )
 
-func GetNovel(novel string) (any, error) {
-	// title := strings.ReplaceAll(novel, " ", "_")
-
-	NovelSchema, err := repository.GetNovel(novel)
+func CreateNovel(novel domain.Novel) error {
+	tableExists, err := table.IsTableCreated(novel.Title)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return NovelSchema, nil
+	if tableExists {
+		return errors.New("Novel already exists")
+	}
+
+	id, err := auth_service.GenerateID("novel")
+	if err != nil {
+		return err
+	}
+
+	novel.ID = id
+
+	err = table.CreateTable(novel.ID)
+	if err != nil {
+		return err
+	}
+
+	novel.CreatedAt = time.Now().Format("2006-01-02 15:04:05")
+	novel.UpdatedAt = novel.CreatedAt
+	novel.UploadedAt = novel.CreatedAt
+
+	err = repository.CreateNovel(novel)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetNovel(id string) (domain.Novel, error) {
+	// title := strings.ReplaceAll(novel, " ", "_")
+
+	novel, err := repository.GetNovel(id)
+	if err != nil {
+		return domain.Novel{}, err
+	}
+
+	return novel, nil
 }
 
 func GetAllNovels() (any, error) {
@@ -23,14 +60,14 @@ func GetAllNovels() (any, error) {
 		return nil, err
 	}
 
-	NovelDTOs, ok := result.([]domain.NovelDTO)
+	Novels, ok := result.([]domain.Novel)
 	if !ok {
 		return nil, errors.New("Type assertion failed")
 	}
 
 	var novels []domain.Novel
-	for _, dto := range NovelDTOs {
-		novels = append(novels, dto.Novel)
+	for _, novel := range Novels {
+		novels = append(novels, novel)
 	}
 
 	return novels, nil
