870ceda8eec71f763acdad63d17c990f34d1cee6
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2025-07-15 12:53:51 +0400

    Minor modifications.

--- api/internal/interfaces/rest/handlers/chapter_handler.go
+++ api/internal/interfaces/rest/handlers/chapter_handler.go
@@ -9,39 +9,38 @@ import (
 )
 
 func FindChapter(c *gin.Context) {
-	novel := c.Param("novel")
-	title := c.Param("chapter")
+	novelId := c.Param("novel")
+	titleId := c.Param("chapter")
 
-	chapter, err := chapter_service.GetChapter(novel, title)
+	chapter, err := chapter_service.GetChapter(novelId, titleId)
 	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{
 			"error": err.Error(),
-		},
-		)
+		})
 		return
 	}
 
-	c.JSON(http.StatusOK,
-		gin.H{"chapter": chapter},
-	)
+	c.JSON(http.StatusOK, gin.H{
+		"chapter": chapter,
+	})
 }
 
 func FindPreviousAndNextChapters(c *gin.Context) {
-	novel := c.Param("novel")
-	title := c.Param("chapter")
+	novelId := c.Param("novel")
+	chapterId := c.Param("chapter")
 
-	chapters, err := chapter_service.GetAllChapters(novel)
+	chapters, err := chapter_service.GetAllChapters(novelId)
 	if err != nil {
-		c.JSON(http.StatusNotFound,
-			gin.H{"error": err.Error()},
-		)
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": err.Error(),
+		})
 		return
 	}
 
 	prev_next := []domain.Chapter{}
 
 	for i, chapter := range chapters {
-		if chapter.Title == title {
+		if chapter.Title == chapterId {
 			if i > 0 {
 				prev_next = append(prev_next, chapters[i-1])
 			}
@@ -53,15 +52,15 @@ func FindPreviousAndNextChapters(c *gin.Context) {
 		}
 	}
 
-	c.JSON(http.StatusOK,
-		gin.H{"chapters": prev_next},
-	)
+	c.JSON(http.StatusOK, gin.H{
+		"chapters": prev_next,
+	})
 }
 
 func FindAllChapters(c *gin.Context) {
-	novel := c.Param("novel")
+	novelId := c.Param("novel")
 
-	chapters, err := chapter_service.GetAllChapters(novel)
+	chapters, err := chapter_service.GetAllChapters(novelId)
 	if err != nil {
 		c.JSON(http.StatusNotFound,
 			gin.H{"error": err.Error()},
@@ -69,7 +68,7 @@ func FindAllChapters(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK,
-		gin.H{"chapters": chapters},
-	)
+	c.JSON(http.StatusOK, gin.H{
+		"chapters": chapters,
+	})
 }
