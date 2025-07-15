97313041d1bc98fc49dd731869fa779e9f4b09f7
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2025-07-15 20:43:00 +0400

    Added code for user retrieval.
    By email or by ID.

--- api/internal/infrastructure-firestore/collections/users.go
+++ api/internal/infrastructure-firestore/collections/users.go
@@ -3,13 +3,49 @@ package firestore_collections
 import (
 	"Codex-Backend/api/internal/domain"
 	"context"
+	"errors"
 )
 
 func (c *Client) createUser(user domain.User, ctx context.Context) error {
-	_, err := c.Client.Collection("Users").Doc(user.ID).Set(ctx, user)
+	_, err := c.Client.Collection("users").Doc(user.ID).Set(ctx, user)
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func (c *Client) getUserByEmail(email string, ctx context.Context) (*domain.User, error) {
+	doc, err := c.Client.Collection("users").Documents(ctx).GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var user domain.User
+	for _, d := range doc {
+		if d.Data()["email"] == email {
+			err = d.DataTo(&user)
+			if err != nil {
+				return nil, err
+			}
+			return &user, nil
+		}
+	}
+
+	return nil, errors.New("User not found")
+}
+
+func (c *Client) getUserById(id string, ctx context.Context) (*domain.User, error) {
+	doc, err := c.Client.Collection("users").Doc(id).Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var user domain.User
+	err = doc.DataTo(&user)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
