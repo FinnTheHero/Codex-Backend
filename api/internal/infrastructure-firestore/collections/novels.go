7100b7967a3c81bec74f227b7daccbe07b80f266
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2025-07-15 22:37:49 +0400

    Added novel find functions.
    Added function to find all novels and find a specific novel by id.
    Renamed a createNovelDocument function to createNovel.

--- api/internal/infrastructure-firestore/collections/novels.go
+++ api/internal/infrastructure-firestore/collections/novels.go
@@ -11,11 +11,43 @@ type Client struct {
 	*firestore.Client
 }
 
-func (c *Client) createNovelDocument(novel domain.Novel, ctx context.Context) error {
+func (c *Client) createNovel(novel domain.Novel, ctx context.Context) error {
 	_, err := c.Client.Collection("novels").Doc(novel.ID).Set(ctx, novel)
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func (c *Client) getNovelById(id string, ctx context.Context) (domain.Novel, error) {
+	doc, err := c.Client.Collection("novels").Doc(id).Get(ctx)
+	if err != nil {
+		return domain.Novel{}, err
+	}
+
+	novel := domain.Novel{}
+	if err := doc.DataTo(&novel); err != nil {
+		return domain.Novel{}, err
+	}
+
+	return novel, nil
+}
+
+func (c *Client) getAllNovels(ctx context.Context) (*[]domain.Novel, error) {
+	doc, err := c.Client.Collection("novels").Documents(ctx).GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	novels := []domain.Novel{}
+	for _, d := range doc {
+		novel := domain.Novel{}
+		if err := d.DataTo(&novel); err != nil {
+			return nil, err
+		}
+		novels = append(novels, novel)
+	}
+
+	return &novels, nil
 }
