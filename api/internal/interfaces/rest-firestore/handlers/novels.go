45a51eb864fa9fc77a9f28dee4b6d4157a82e853
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2025-07-17 15:59:26 +0400

    Created Novel handler.

---
+++ api/internal/interfaces/rest-firestore/handlers/novels.go
@@ -0,0 +1,101 @@
+package firestore_handlers
+
+import (
+	"Codex-Backend/api/internal/common"
+	"Codex-Backend/api/internal/domain"
+	firestore_services "Codex-Backend/api/internal/usecases-firestore/collections"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func FindNovel(c *gin.Context) {
+	ctx := c.Request.Context()
+	defer ctx.Done()
+
+	result, ok := c.Get("novel")
+	if !ok {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "Novel ID not found",
+		})
+		return
+	}
+
+	novelId, ok := result.(string)
+	if !ok {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "Novel ID not found",
+		})
+		return
+	}
+
+	novel, err := firestore_services.GetNovel(novelId, ctx)
+	if e, ok := err.(*common.Error); ok {
+		c.AbortWithStatusJSON(e.StatusCode(), gin.H{
+			"error": "Failed to retrieve novel: " + e.Error(),
+		})
+		return
+	} else if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to retrieve novel: " + err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"novel": novel,
+	})
+}
+
+func FindAllNovels(c *gin.Context) {
+	ctx := c.Request.Context()
+	defer ctx.Done()
+
+	novels, err := firestore_services.GetAllNovels(ctx)
+	if e, ok := err.(*common.Error); ok {
+		c.AbortWithStatusJSON(e.StatusCode(), gin.H{
+			"error": "Failed to retrieve novels: " + e.Error(),
+		})
+		return
+	} else if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to retrieve novels: " + err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"novels": novels,
+	})
+}
+
+func CreateNovel(c *gin.Context) {
+	ctx := c.Request.Context()
+	defer ctx.Done()
+
+	novel := domain.Novel{}
+
+	if err := c.ShouldBindJSON(&novel); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to get novel data: " + err.Error(),
+		})
+		return
+	}
+
+	err := firestore_services.CreateNovel(novel, ctx)
+	if e, ok := err.(*common.Error); ok {
+		c.AbortWithStatusJSON(e.StatusCode(), gin.H{
+			"error": "Failed to create novel: " + e.Error(),
+		})
+		return
+	} else if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to create novel: " + err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Novel created successfully",
+	})
+}
