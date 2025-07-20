088ee0ea1292d808206e10f58638e5c6adc9253b
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2025-07-20 16:46:38 +0400

    Updated error handling for fetching evn variables.

--- api/internal/common/env.go
+++ api/internal/common/env.go
@@ -1,7 +1,8 @@
 package common
 
 import (
-	"fmt"
+	"errors"
+	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -14,7 +15,7 @@ func LoadEnvVariables() error {
 func GetEnvVariable(v string) (string, error) {
 	env_variable := os.Getenv(v)
 	if env_variable == "" {
-		return "", fmt.Errorf("Environmental Variable \"%v\" Not Found", v)
+		return "", &Error{Err: errors.New("Environmental Variable" + v + " Not Found"), Status: http.StatusNotFound}
 	}
 
 	return env_variable, nil
