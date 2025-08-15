fcab2e5221285cd18123ca67503d8e234ca6f9f9
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2025-08-16 03:32:22 +0400

    Refactor: Simplify FrontendChapter struct

--- api/internal/domain/chapter.go
+++ api/internal/domain/chapter.go
@@ -29,11 +29,8 @@ type Chapter struct {
 
 // Chapter struct used on frontend
 type FrontendChapter struct {
-	ID          string `json:"id"`
-	Title       string `json:"title"`
-	Author      string `json:"author"`
-	Description string `json:"description"`
-	CreatedAt   string `json:"creation_date"`
-	UpdatedAt   string `json:"update_date"`
-	Content     string `json:"content"`
+	ID        string `json:"id"`
+	Title     string `json:"title"`
+	UpdatedAt string `json:"update_date"`
+	Content   string `json:"content"`
 }
