de6397bddc26094a1be7e82784accf27cd81a1a9
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2025-08-18 22:54:54 +0400

    feat: Implement EPUB processing worker

--- api/cmd/codex/worker/main.go
+++ api/cmd/codex/worker/main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
-	"fmt"
+	cmn "Codex-Backend/api/internal/common"
+	firestore_services "Codex-Backend/api/internal/usecases/collections"
+	"Codex-Backend/api/internal/usecases/worker"
+	"context"
+	"log"
+	"os"
+
+	"github.com/riverqueue/river"
 )
 
+func init() {
+	if mode := os.Getenv("GIN_MODE"); mode == "debug" {
+		cmn.LoadEnvVariables()
+	}
+}
+
 func main() {
-	fmt.Println("Worker started")
+	riverClient := cmn.InitializeRiverClient(context.Background())
+	if err := riverClient.Start(context.Background()); err != nil {
+		log.Fatal(err)
+	}
+
+	workers := river.NewWorkers()
+	river.AddWorker(workers, &EPUBWorker{})
+}
+
+type EPUBWorker struct {
+	river.WorkerDefaults[worker.ProcessEPUBArgs]
+}
+
+func (w *EPUBWorker) Work(ctx context.Context, job *river.Job[worker.ProcessEPUBArgs]) error {
+	return firestore_services.CreateNovelFromEPUB(job.Args.File, ctx)
 }
