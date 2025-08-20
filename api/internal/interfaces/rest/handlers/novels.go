3bc63b4ae661b061734838a741056a2c1baa1697
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2025-08-20 21:44:13 +0400

    Removed commented code

--- api/internal/interfaces/rest/handlers/novels.go
+++ api/internal/interfaces/rest/handlers/novels.go
@@ -61,19 +61,6 @@ func EPUBNovel(c *gin.Context) {
 		return
 	}
 
-	// err = firestore_services.CreateNovelFromEPUB(epubFile, ctx)
-	// if e, ok := err.(*cmn.Error); ok {
-	// 	c.AbortWithStatusJSON(e.StatusCode(), gin.H{
-	// 		"error": "Failed to upload EPUB file: " + e.Error(),
-	// 	})
-	// 	return
-	// } else if err != nil {
-	// 	c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
-	// 		"error": "Failed to upload EPUB file: " + err.Error(),
-	// 	})
-	// 	return
-	// }
-
 	c.JSON(http.StatusOK, gin.H{
 		"message": "EPUB file uploaded successfully",
 	})
